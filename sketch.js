f8cdd2f627b255ac291dfc8d7edc75782b73e611
Kanchan <70108103+kanchan0912@users.noreply.github.com>
2020-09-15 08:39:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -31,7 +31,8 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.6, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -57,9 +58,15 @@ function draw() {
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
     // Look at the hints in the document and understand how to make the package body fall only on
-    
+    if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(packageBody,false);
+		
+	  }
   }
 }
+
+
+
 
 
 
